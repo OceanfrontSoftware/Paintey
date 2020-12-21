f2ce285ffe94f1f0d349d47d0e0e591dda3aa975
Nathan <ntownsend@bayshoresolutions.com>
2020-12-20 21:26:32 -0500

    made the canvas 100% and hide toolbar when drawing

---
+++ app/app.js
@@ -0,0 +1,21 @@
+
+// a collection of all draw events
+var history = {
+    
+    // all draw events for canvas 0
+    layer0: [
+        {
+            pluginId: "d861778c-12c7-4563-baa4-3f0fc5bd7132",
+            x: 150,
+            y: 290,
+            size: 20,
+            arc: {
+                start: 0,
+                stop: 360
+            },
+            strokeColor: '#647',
+            strokeThickness: 1,
+            fillColor: '#295'
+        }
+    ]
+}
