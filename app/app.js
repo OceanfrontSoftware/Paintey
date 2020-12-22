e024a0eb68d8c6b8c0f35c40ac65b4e96ab0cca4
Nathan <ntownsend@bayshoresolutions.com>
2020-12-22 08:22:53 -0500

    built out the app folder with layout, buttons, and popup

--- app/app.js
+++ app/app.js
@@ -1,21 +1,26 @@
+/* OPEN MODAL FORM EDITORS */
+$(document).ready(function(){
+    $("button[data-action]").click(openActionHandler);
+    $(".closeButton").click(closeActionHandler)
+})
 
-// a collection of all draw events
-var history = {
-    
-    // all draw events for canvas 0
-    layer0: [
-        {
-            pluginId: "d861778c-12c7-4563-baa4-3f0fc5bd7132",
-            x: 150,
-            y: 290,
-            size: 20,
-            arc: {
-                start: 0,
-                stop: 360
-            },
-            strokeColor: '#647',
-            strokeThickness: 1,
-            fillColor: '#295'
-        }
-    ]
+
+function openActionHandler(event){
+    event.preventDefault();
+    var button = $(this);
+    var action = button.attr('data-action');
+    $("#form-container").removeClass('hide');
+    $("#"+ action).removeClass('hide');
+    $("#"+ action).addClass('show');
 }
+
+function closeActionHandler(){
+    console.log('close');
+    var form = $(".show");
+    var action = form.attr('id');
+    $("#form-container").addClass('hide');
+    $("#"+ action).addClass('hide');
+    $("#"+ action).removeClass('show');
+}
+
+
