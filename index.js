9adddc93c7d57925ee6edd36e58c1a6640ded9f7
Nathan <ntownsend@bayshoresolutions.com>
2021-01-11 07:50:34 -0500

    preparing for running under pm2 on server

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ const https = require('https')
 const fs = require('fs')
 const port = 3000
 
+
 app.use(express.static('_static'));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send("IT'S WORKING!")
 })
 
-const httpsOptions = {
-    key: fs.readFileSync('./security/cert.key'),
-    cert: fs.readFileSync('./security/cert.pem')
+console.log(`NODE_ENV=${process.env.NODE_ENV}`);
+
+if(process.env.NODE_ENV == "prod"){
+    app.listen(port, ()=>{
+        console.log(`app is listening on port ${port}`);
+    })
 }
-const server = https.createServer(httpsOptions, app)
-    .listen(port, () => {
-        console.log('server running at ' + port)
-});
+
+if(process.env.NODE_ENV === "dev"){
+    console.log('here');
+    const httpsOptions = {
+        key: fs.readFileSync('./security/cert.key'),
+        cert: fs.readFileSync('./security/cert.pem')
+    }
+    const server = https.createServer(httpsOptions, app)
+        .listen(port, () => {
+            console.log('server running at ' + port)
+    });
+}
