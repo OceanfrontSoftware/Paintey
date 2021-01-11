3932777008ef65cf15cd7a4aa3a8406dafdbf323
Nathan <ntownsend@bayshoresolutions.com>
2021-01-11 08:23:31 -0500

    updates for prod env

--- index.js
+++ index.js
@@ -11,22 +11,19 @@ app.get('/test', (req, res) => {
     res.send("IT'S WORKING!")
 })
 
-console.log(`NODE_ENV=${process.env.NODE_ENV}`);
-
-if(process.env.NODE_ENV == "prod"){
-    app.listen(port, ()=>{
-        console.log(`app is listening on port ${port}`);
-    })
-}
-
 if(process.env.NODE_ENV === "dev"){
-    console.log('here');
+    console.log('running dev');
     const httpsOptions = {
         key: fs.readFileSync('./security/cert.key'),
         cert: fs.readFileSync('./security/cert.pem')
     }
     const server = https.createServer(httpsOptions, app)
         .listen(port, () => {
-            console.log('server running at ' + port)
+            console.log('server running in DEV mode on ' + port)
     });
+} else {
+    console.log('running prod');
+    app.listen(port, ()=>{
+        console.log('server running in PROD mode on ' + port)
+    })
 }
