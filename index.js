51f27f6772ec3789f262ef277aecbe76c5b65bf1
Nathan <ntownsend@bayshoresolutions.com>
2021-01-11 20:43:37 -0500

    fixing port number problem

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const https = require('https')
 const fs = require('fs')
-const port = 3001
+const port = 3000
 const imageRouteHandler = require('./routes/image.js')
 
 
